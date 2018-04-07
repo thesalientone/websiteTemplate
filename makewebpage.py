d80ad4889310337cea78c2968fa5cd5a996042d6
Stefan Jenkins <stefan.jenkins@gmail.com>
2018-04-07 08:20:25 -0400

    Modified to include os.getcwd

--- makewebpage.py
+++ makewebpage.py
@@ -1,6 +1,8 @@
 from distutils.dir_util import copy_tree
+from os import getcwd
 from sys import argv
 
 destinationFolder = argv[1]
-
-copy_tree('./template', destinationFolder)
+pathName = getcwd()
+pathName += '/template'
+copy_tree(pathName, destinationFolder)
